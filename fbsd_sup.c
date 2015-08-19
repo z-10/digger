93d7fd2c2f201eedd107179c07569203741aaec1
Stetsyshin, Roman <roman.stetsyshin@wbgames.com>
2015-08-18 19:53:00 -0700

    Initial commit

---
+++ fbsd_sup.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include "def.h"
+#include "hardware.h"
+
+void strupr(char *str)
+{
+	while(*str != 0) {
+		*str = toupper(*str);
+		str++;
+	}
+}
+
+void catcher(int num) {
+	fprintf(stderr, "Signal %d catched, exitting\n", num);
+	graphicsoff();
+	restorekeyb();
+	exit(0);
+}
+	
+
